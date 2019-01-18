216b9d35930bc85a983d414ad2b42a85866dfdb8
orangepiratee <orangepiratee@protonmail.com>
2019-01-18 04:33:05 -0400

    add some templates

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 
-from flask import Flask
-from flask import make_response
-from flask import abort
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
+from datetime import datetime
+
+class NameForm(FlaskForm):
+    name = StringField('What\'s you name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'nothing'
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None or old_name != form.name.data:
+            flash('yowza, you changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+@app.route('/overview')
+def overview():
+    return render_template('overview.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -27,30 +45,6 @@ def analysis():
 def management():
     return render_template('management.html')
 
-@app.route('/400')
-def bad_request():
-    return '<h1>Bad Request!</h1>',400
-
-@app.route('/cookie')
-def cookie():
-    response = make_response('<h1>this document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
-
-@app.route('/user/<id>')
-def get_user(id):
-    #user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h1>hello {}!</h1>'.format(user.name)
-
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
-@app.route('/500')
-def internal_s_e():
-    return render_template('500.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
